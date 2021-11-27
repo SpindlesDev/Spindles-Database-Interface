0d10a301825ab98e5a0117c0e3747d44208a9dce
Spindles <>
2021-11-27 14:23:01 -0700

    Added JSON read (Forgot to add on first push xD)

--- main.js
+++ main.js
@@ -8,70 +8,72 @@
 \****************************************/
 
 // Testing support
-const sequelizeLibrary = require("../Spindles-Database-Test-Project/node_modules/sequelize") || require("sequelize");
+const sequelizeLibrary =
+	require("../Spindles-Database-Test-Project/node_modules/sequelize") ||
+	require("sequelize");
 
 const fs = require("fs");
 
-let databaseTypeGlobal 					= null;
-databaseLocationGlobal 					= null;
+let databaseTypeGlobal = null;
+databaseLocationGlobal = null;
 
 /**
- * 
- * @param {Object} config The config object to use for the database interface 
+ *
+ * @param {Object} config The config object to use for the database interface
  * @param {string} config.databaseType Type of the database
  * @param {Object} config.databaseOptions The database options to use
  * @param {string} config.databaseOptions.databaseURI The URI of the database to use
  * @param {string} config.databaseOptions.databaseLocation The location of the database to use (JSON and )
  */
 exports.init = function (config) {
-	const databaseType 					= config.databaseType.toLowerCase();
-	databaseTypeGlobal 					= databaseType		
-	const databaseURI   				= config.databaseOptions.databaseURI;
-	const databaseLocation 				= config.databaseOptions.databaseLocation;
-	databaseLocationGlobal 				= databaseLocation
+	const databaseType = config.databaseType.toLowerCase();
+	databaseTypeGlobal = databaseType;
+	const databaseURI = config.databaseOptions.databaseURI;
+	const databaseLocation = config.databaseOptions.databaseLocation;
+	databaseLocationGlobal = databaseLocation;
 
 	switch (databaseType) {
 		case "json":
-		    configDatabaseType 			= "json";
+			configDatabaseType = "json";
 			break;
 
 		case "mysql":
-			configDatabaseType 			= "mysql";
-			sequelizeInstance 			= new Sequelize(databaseURI, {
-				dialect: 'mysql'
+			configDatabaseType = "mysql";
+			sequelizeInstance = new Sequelize(databaseURI, {
+				dialect: "mysql",
 			});
 
 			break;
 
 		case "mariadb":
-			configDatabaseType 			= "mariadb";
-			sequelizeInstance 			= new Sequelize(databaseURI, {
-				dialect: 'mariadb'
+			configDatabaseType = "mariadb";
+			sequelizeInstance = new Sequelize(databaseURI, {
+				dialect: "mariadb",
 			});
 
 			break;
 
 		case "sqlite":
-			configDatabaseType 			= "sqlite";
-			sequelizeInstance 			= new Sequelize({
+			configDatabaseType = "sqlite";
+			sequelizeInstance = new Sequelize({
 				storage: databaseLocation,
-				dialect: 'sqlite'
+				dialect: "sqlite",
 			});
 
 			break;
 
 		case "postgres":
-			configDatabaseType 			= "postgres";
-			sequelizeInstance 			= new Sequelize(databaseURI, {
-				dialect: 'postgres'
+			configDatabaseType = "postgres";
+			sequelizeInstance = new Sequelize(databaseURI, {
+				dialect: "postgres",
 			});
 
 			break;
 
 		case "mssql":
-			configDatabaseType 			= "mssql";
-			sequelizeInstance 			= new Sequelize(databaseURI, {
-				dialect: 'mssql'
+			configDatabaseType = "mssql";
+			sequelizeInstance = new Sequelize(databaseURI, {
+				dialect: "mssql",
 			});
 
 			break;
@@ -79,15 +81,14 @@ exports.init = function (config) {
 };
 
 /**
- * 
+ *
  * @param {Object} dataInput Input data to handle
  * @param {string} dataInput.type Action to do [Overwrite, Read, Write]
  * @param {string} dataInput.object Object to write
- * @returns 
+ * @returns
  */
 exports.interface = function (dataInput) {
-
-	const databaseLocation = databaseLocationGlobal
+	const databaseLocation = databaseLocationGlobal;
 	const databaseType = databaseTypeGlobal;
 	let dataOutput = null;
 	const databaseAction = dataInput.type.toLowerCase();
@@ -115,22 +116,34 @@ exports.interface = function (dataInput) {
 
 		if (databaseAction === "overwrite") {
 			let databaseData = [];
-			console.log(JSON.stringify(databaseData))
+
 			databaseData.push(dataInput.data);
-			fs.writeFileSync(databaseLocation, JSON.stringify(Object(databaseData), null, 2));
-			console.log(JSON.stringify(databaseData))
+			fs.writeFileSync(
+				databaseLocation,
+				JSON.stringify(Object(databaseData), null, 2)
+			);
+
 			dataOutput = databaseData;
 		}
 
 		if (databaseAction === "write") {
 			const databaseData = JSON.parse(fs.readFileSync(databaseLocation));
-			console.log(JSON.stringify(databaseData))
+
 			databaseData.push(dataInput.data);
-			fs.writeFileSync(databaseLocation, JSON.stringify(Object(databaseData), null, 2));
-			console.log(JSON.stringify(databaseData))
+			fs.writeFileSync(
+				databaseLocation,
+				JSON.stringify(Object(databaseData), null, 2)
+			);
+
+			dataOutput = databaseData;
+		}
+
+		if (databaseAction === "read") {
+			const databaseData = JSON.parse(fs.readFileSync(databaseLocation));
+
 			dataOutput = databaseData;
 		}
 	}
 
-	return dataOutput
-}
+	return dataOutput;
+};
